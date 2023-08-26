8e037c1651a582517d100d0857d62f39ba75c2f6
daisyalexandria <dazavala7@gmail.com>
2023-08-26 17:47:19 -0500

    first 5 skills from skills spreadsheet

---
+++ practice.rb
@@ -0,0 +1,38 @@
+### Create an if statement that uses elsif and else with valid syntax from memory ###
+number = 1
+if number == 1
+  p "yay!"
+else 
+  p "boooo"
+end 
+
+
+### Insert a variable into a string using either concatenation or interpolation from memory ###
+string = "is my favorite cat"
+new_cat = "salem"
+
+p "#{new_cat} #{string}"
+
+
+### Use built-in methods for strings and numbers ###
+# puts "Where is she going this weekend?"
+# word = gets.chomp 
+# if word.downcase == "boystown"
+#   p "purrrrr"
+# else 
+#   p "dumb"
+# end
+
+
+### Create a loop which modifies a variable each time the loop runs from memory ###
+# number = 1
+# while number < 10 
+#   number += 1
+#   p number
+# end
+
+### Create an array, read items from an array, and add new items to existing array from memory ###
+array = [1, 2, 3, 4, 5]
+array.each do |number|
+  puts number
+end 
