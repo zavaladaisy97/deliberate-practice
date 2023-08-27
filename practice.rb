6b32031bd8f205df896e829487144c9da213b1c2
daisyalexandria <dazavala7@gmail.com>
2023-08-27 15:12:48 -0500

    skills patractice 5 and 6

--- practice.rb
+++ practice.rb
@@ -32,7 +32,27 @@ p "#{new_cat} #{string}"
 # end
 
 ### Create an array, read items from an array, and add new items to existing array from memory ###
-array = [1, 2, 3, 4, 5]
-array.each do |number|
-  puts number
-end 
+# array = [1, 2, 3, 4, 5]
+# array.each do |number|
+#   puts number
+# end 
+
+# array = [1]
+# array << 2
+# array << 3
+
+# p array
+
+### Create a hash, read items from a hash, and add new items to existing hash from memory ###
+
+new_employee = {
+  "first_name" => "Tony",
+  "last_name" => "Perez",
+  "position" => "host"
+}
+
+p new_employee["first_name"]
+
+new_employee["age"] = "26"
+
+p new_employee
